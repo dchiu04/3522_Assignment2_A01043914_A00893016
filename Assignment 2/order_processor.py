3c986d302370333b92d56294c4992bd8a1f7f4e0
norman-c <nwpchiu@gmail.com>
2020-03-09 18:26:56 -0700

    Debby doo doo

--- Assignment 2/order_processor.py
+++ Assignment 2/order_processor.py
@@ -1,2 +1,17 @@
+import pandas as pd
+
+
 class OrderProcessor:
-    pass
+
+    @staticmethod
+    def readFileToOrders(fileName):
+        allOrders = pd.read_excel(fileName).to_dict(orient="record")
+        for i in allOrders:
+            print(i)
+
+def main():
+    OrderProcessor.readFileToOrders("orders.xlsx")
+
+
+if __name__ == '__main__':
+    main()

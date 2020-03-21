4360a10af7259e7266261134e372a81a7e6155f5
normanc <unconfigured@null.spigotmc.org>
2020-03-21 14:47:04 -0700

    Debbie dum dum

--- Assignment 2/amazon.py
+++ Assignment 2/amazon.py
@@ -4,54 +4,84 @@ import datetime
 
 class Amazon:
     def __init__(self):
-        self._inventory = []
+        self._inventory = {}
 
     def process_orders(self):
         user = input("Enter the name of the excel file to be processed(.xlsx): ")
         op = OrderProcessor()
         orders = op.read_file_to_orders(user + ".xlsx")
 
-        print("SHOULD BE PRINTING ORDERS")
-        print(orders)
+        # print("SHOULD BE PRINTING ORDERS")
+        # print(orders)
         for i in orders:
-            kwargs = {'name': orders[i].details['name'],
-                      'desc': orders[i].details['description'],
-                      'prod_id': orders[i]._prod_id,
-                      'has_batteries': orders[i].details['has_batteries'],
-                      'min_age': orders[i].details['min_age'],
-                      'dimensions': orders[i].details['dimensions'],
-                      'num_rooms': orders[i].details['num_rooms'],
-                      'speed': orders[i].details['speed'],
-                      'jump_height': orders[i].details['jump_height'],
-                      'has_glow': orders[i].details['has_glow'],
-                      'spider_type': orders[i].details['spider_type'],
-                      'num_sound': orders[i].details['num_sound'],
-                      'colour': orders[i].details['colour'],
-                      'has_lactose': orders[i].details['has_lactose'],
-                      'has_nuts': orders[i].details['has_nuts'],
-                      'variety': orders[i].details['variety'],
-                      'pack_size': orders[i].details['pack_size'],
-                      'stuffing': orders[i].details['stuffing'],
-                      'size': orders[i].details['size'],
-                      'fabric': orders[i].details['fabric']
+            kwargs = {'name': orders[i][1].name,
+                      'desc': orders[i][1].details['description'],
+                      'product_id': orders[i][1].product_id,
+                      'has_batteries': orders[i][1].details['has_batteries'],
+                      'min_age': orders[i][1].details['min_age'],
+                      'dimensions': orders[i][1].details['dimensions'],
+                      'num_rooms': orders[i][1].details['num_rooms'],
+                      'speed': orders[i][1].details['speed'],
+                      'jump_height': orders[i][1].details['jump_height'],
+                      'has_glow': orders[i][1].details['has_glow'],
+                      'spider_type': orders[i][1].details['spider_type'],
+                      'num_sound': orders[i][1].details['num_sound'],
+                      'colour': orders[i][1].details['colour'],
+                      'has_lactose': orders[i][1].details['has_lactose'],
+                      'has_nuts': orders[i][1].details['has_nuts'],
+                      'variety': orders[i][1].details['variety'],
+                      'pack_size': orders[i][1].details['pack_size'],
+                      'stuffing': orders[i][1].details['stuffing'],
+                      'size': orders[i][1].details['size'],
+                      'fabric': orders[i][1].details['fabric']
                       }
 
             # key is the product id, value is the quantity
+            # print(item)
+
             item = orders[i][1].factory.create(orders[i][1].factory, **kwargs)
-            print(item)
-
             # CREATE 100 OF EACH ORDER ITEM HERE. old method no work
-            # # Initializing inventory quantity
-            # if self.init_quantity(item, orders[i][0]):
-            #     temp = self._inventory[item]
-            #     self._inventory[item] = temp - orders[i][0]
-            # else:
-            #     self._inventory[item] = 100 - orders[i][0]
+            # Initializing inventory quantity
+            print(item.product_id)
+            try:
+                if self.check_quantity(item.product_id, orders[i][0]):
+                    for x in range(orders[i][0]):
+                        self._inventory[item.product_id].remove(item)
+                else:
+                    for x in range(100 - orders[i][0]):
+                        self._inventory[item.product_id].append(item)
+            except KeyError:
+                self._inventory[item.product_id] = []
+                for x in range(100 - orders[i][0]):
+                    self._inventory[item.product_id].append(item)
 
-        for (key, value) in self._inventory:
-            print(key, value)
+        # for (key, value) in self._inventory:
+        #     print(key, value)
         #     order.factory.create(kwargs)
         print("Successfully processed orders.")
+
+    def check_quantity(self, product_id, quantity_ordered):
+        quantity = self._inventory[product_id].length
+        if quantity > quantity_ordered:
+            return True
+        else:
+            return False
+        return False
+
+
+    # def init_quantity(self, product_id, quantity_ordered):
+    #     quantity = 0
+    #     try:
+    #         quantity = self._inventory[product_id][0]
+    #     except TypeError:
+    #         quantity = 0
+    #     except KeyError:
+    #         quantity = 0
+    #     finally:
+    #         if quantity > quantity_ordered:
+    #             return True
+    #         else:
+    #             return False
 
     def restock_inv(self, prod_id):
         for i in self._inventory:
@@ -67,20 +97,6 @@ class Amazon:
                 #     if prod_id == i:
                 #         if i.value == 0:
                 #             self._inventory[i].value += 100
-
-    def init_quantity(self, product_id, quantity_ordered):
-        quantity = 0
-        try:
-            quantity = self._inventory[product_id][0]
-        except TypeError:
-            quantity = 0
-        except KeyError:
-            quantity = 0
-        finally:
-            if quantity > quantity_ordered:
-                return True
-            else:
-                return False
 
     def check_inv(self):
         for i in self._inventory:
@@ -121,8 +137,10 @@ class Amazon:
 def main():
     store = Amazon()
     store.menu()
-    for i in store._inventory:
-        print(i._name)
+    for i, k in store._inventory.items():
+        for x in k:
+            print(x)
+
 
 
 if __name__ == '__main__':
--- Assignment 2/animal.py
+++ Assignment 2/animal.py
@@ -1,47 +1,64 @@
 class Animal:
-    def __init__(self, stuffing, size, fabric, name, desc, prod_id):
+    def __init__(self, stuffing, size, fabric, name, desc, product_id):
         self._stuffing = stuffing
         self._size = size
         self._fabric = fabric
         self._name = name
         self._desc = desc
-        self._prod_id = prod_id
+        self._product_id = product_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def product_id(self):
+        return self._product_id
+
+    @property
+    def desc(self):
+        return self._desc
+
+    def __str__(self):
+        return f"Name: {self._name} ----\n" \
+               f"Product ID: {self._product_id}\n" \
+               f"Description: {self._desc}\n"
 
 
 class Skeleton(Animal):
     def __init__(self, has_glow, **kwargs):
         super().__init__(
-                         kwargs.get("stuffing"),
-                         kwargs.get("size"),
-                         kwargs.get("fabric"),
-                         kwargs.get("name"),
-                         kwargs.get("desc"),
-                         kwargs.get("prod_id")
-                         )
+            kwargs.get("stuffing"),
+            kwargs.get("size"),
+            kwargs.get("fabric"),
+            kwargs.get("name"),
+            kwargs.get("desc"),
+            kwargs.get("product_id")
+        )
         self._has_glow = has_glow
 
 
 class Reindeer(Animal):
     def __init__(self, has_glow, **kwargs):
         super().__init__(
-                         kwargs.get("stuffing"),
-                         kwargs.get("size"),
-                         kwargs.get("fabric"),
-                         kwargs.get("name"),
-                         kwargs.get("desc"),
-                         kwargs.get("prod_id")
-                         )
+            kwargs.get("stuffing"),
+            kwargs.get("size"),
+            kwargs.get("fabric"),
+            kwargs.get("name"),
+            kwargs.get("desc"),
+            kwargs.get("product_id")
+        )
         self._has_glow = has_glow
 
 
 class Bunny(Animal):
     def __init__(self, colour, **kwargs):
         super().__init__(
-                         kwargs.get("stuffing"),
-                         kwargs.get("size"),
-                         kwargs.get("fabric"),
-                         kwargs.get("name"),
-                         kwargs.get("desc"),
-                         kwargs.get("prod_id")
-                         )
+            kwargs.get("stuffing"),
+            kwargs.get("size"),
+            kwargs.get("fabric"),
+            kwargs.get("name"),
+            kwargs.get("desc"),
+            kwargs.get("product_id")
+        )
         self._colour = colour

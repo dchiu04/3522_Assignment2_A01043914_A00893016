96872f3ba9ce601751400f6d38edc160393905dd
debby <debbychiu@outlook.com>
2020-03-21 12:54:55 -0700

    changed process_orders and order_processor

--- Assignment 2/order.py
+++ Assignment 2/order.py
@@ -8,38 +8,34 @@ class Order:
         self._prod_details = prod_details
         self._factory = None
 
-    @property
-    def order_num(self):
-        return self._order_num
-
-    @property
-    def type(self):
-        return self._item_type
-
-    @property
-    def prod_id(self):
-        return self._prod_id
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def details(self):
-        return self._prod_details
-
-    @property
-    def factory(self):
-        return self._factory
+    # @property
+    # def order_num(self):
+    #     return self._order_num
+    #
+    # @property
+    # def type(self):
+    #     return self._item_type
+    #
+    # @property
+    # def prod_id(self):
+    #     return self._prod_id
+    #
+    # @property
+    # def name(self):
+    #     return self._name
+    #
+    # @property
+    # def details(self):
+    #     return self._prod_details
+    #
+    # @property
+    # def factory(self):
+    #     return self._factory
 
     def __str__(self):
-        # return "Order: {}, Item {}, Product ID {}, Name ""{}"", Quantity {}" \
-        #     .format(self.order(), self.type(), self.prod_id(),
-        #             self.name(), 100)
-
-        return f"---- Order: {self.order_num} ----\n" \
-               f"Item: {self.type}\n" \
-               f"Product ID: {self.prod_id}\n" \
-               f"Name: {self.name}\n" \
+        return f"---- Order: {self._order_num} ----\n" \
+               f"Item: {self._item_type}\n" \
+               f"Product ID: {self._prod_id}\n" \
+               f"Name: {self._name}\n" \
                f"Details: {self._prod_details}\n" \
                f"Factory: {self._factory}"

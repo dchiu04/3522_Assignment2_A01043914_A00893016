475cc22a881587e2bd105d9594d4b893af13fbe6
debby <debbychiu@outlook.com>
2020-03-21 16:31:04 -0700

    commiting to pull

--- Assignment 2/amazon.py
+++ Assignment 2/amazon.py
@@ -5,39 +5,40 @@ import datetime
 class Amazon:
     def __init__(self):
         self._inventory = []
+        self._orders = []
 
     def process_orders(self):
         user = input("Enter the name of the excel file to be processed(.xlsx): ")
         op = OrderProcessor()
-        orders = op.read_file_to_orders(user + ".xlsx")
+        self._orders = op.read_file_to_orders(user + ".xlsx")
 
         print("SHOULD BE PRINTING ORDERS")
-        print(orders)
-        for i in orders:
-            kwargs = {'name': orders[i].details['name'],
-                      'desc': orders[i].details['description'],
-                      'prod_id': orders[i]._prod_id,
-                      'has_batteries': orders[i].details['has_batteries'],
-                      'min_age': orders[i].details['min_age'],
-                      'dimensions': orders[i].details['dimensions'],
-                      'num_rooms': orders[i].details['num_rooms'],
-                      'speed': orders[i].details['speed'],
-                      'jump_height': orders[i].details['jump_height'],
-                      'has_glow': orders[i].details['has_glow'],
-                      'spider_type': orders[i].details['spider_type'],
-                      'num_sound': orders[i].details['num_sound'],
-                      'colour': orders[i].details['colour'],
-                      'has_lactose': orders[i].details['has_lactose'],
-                      'has_nuts': orders[i].details['has_nuts'],
-                      'variety': orders[i].details['variety'],
-                      'pack_size': orders[i].details['pack_size'],
-                      'stuffing': orders[i].details['stuffing'],
-                      'size': orders[i].details['size'],
-                      'fabric': orders[i].details['fabric']
+        print(self._orders)
+        for i in self._orders:
+            kwargs = {'name': self._orders[i].details['name'],
+                      'desc': self._orders[i].details['description'],
+                      'prod_id': self._orders[i]._prod_id,
+                      'has_batteries': self._orders[i].details['has_batteries'],
+                      'min_age': self._orders[i].details['min_age'],
+                      'dimensions': self._orders[i].details['dimensions'],
+                      'num_rooms': self._orders[i].details['num_rooms'],
+                      'speed': self._orders[i].details['speed'],
+                      'jump_height': self._orders[i].details['jump_height'],
+                      'has_glow': self._orders[i].details['has_glow'],
+                      'spider_type': self._orders[i].details['spider_type'],
+                      'num_sound': self._orders[i].details['num_sound'],
+                      'colour': self._orders[i].details['colour'],
+                      'has_lactose': self._orders[i].details['has_lactose'],
+                      'has_nuts': self._orders[i].details['has_nuts'],
+                      'variety': self._orders[i].details['variety'],
+                      'pack_size': self._orders[i].details['pack_size'],
+                      'stuffing': self._orders[i].details['stuffing'],
+                      'size': self._orders[i].details['size'],
+                      'fabric': self._orders[i].details['fabric']
                       }
 
             # key is the product id, value is the quantity
-            item = orders[i][1].factory.create(orders[i][1].factory, **kwargs)
+            item = self._orders[i][1].factory.create(self._orders[i][1].factory, **kwargs)
             print(item)
 
             # CREATE 100 OF EACH ORDER ITEM HERE. old method no work

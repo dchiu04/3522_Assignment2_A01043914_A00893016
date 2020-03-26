870656d6768c7723efb7cc949c88b13d7e3c2575
normanc <unconfigured@null.spigotmc.org>
2020-03-25 19:46:15 -0700

    debbie guy

--- Lab 10/producer_consumer.py
+++ Lab 10/producer_consumer.py
@@ -1,19 +1,14 @@
 import threading
+import time
 
 import city_processor
-from consumer_thread import ConsumerThread
-from producer_thread import ProducerThread
-
-
-class ProducerConsumer:
-    pass
 
 
 class CityOverheadTimeQueue:
 
     def __init__(self):
         self._data_queue = []
-        self._access_queue_lock = threading.Lock() # Not sure about this line, he wrote it in an example tho
+        self._access_queue_lock = threading.Lock()  # Not sure about this line, he wrote it in an example tho
 
     def put(self, overhead_time: city_processor.CityOverheadTimes) -> None:
         with self._access_queue_lock:
@@ -29,6 +24,44 @@ class CityOverheadTimeQueue:
         return len(self._data_queue)
 
 
+class ProducerThread(threading.Thread):
+    def __init__(self, cities: list, queue: CityOverheadTimeQueue):
+        super().__init__()
+        self._cities = cities
+        self._queue = queue
+
+    def run(self) -> None:
+        count = 0
+
+        for c in self._cities:
+            if count < 5:
+                self._queue.put(city_processor.ISSDataRequest.get_overhead_pass(c))
+                count += 1
+            else:
+                count = 0
+                time.sleep(1)
+
+
+class ConsumerThread(threading.Thread):
+
+    def __init__(self, queue: CityOverheadTimeQueue):
+        super().__init__()
+        self._queue = queue
+        self._data_incoming = True
+
+    def run(self) -> None:
+        while self._data_incoming or self._queue.len() > 0:
+            try:
+                item = self._queue.get()
+            except IndexError:
+                item = None
+            print(item)
+            if self._queue.len() == 0:
+                time.sleep(0.75)
+            else:
+                time.sleep(0.5)
+
+
 def main():
     filepath = "city_locations.xlsx"
     db = city_processor.CityDatabase(filepath)
@@ -38,10 +71,13 @@ def main():
     city = db.city_db[1]
     print(city)
     city_overhead1 = city_processor.ISSDataRequest.get_overhead_pass(city)
-    q = CityOverheadTimeQueue()
 
-    prod = ProducerThread(city, q)
-    cons = ConsumerThread(city)
+    q = CityOverheadTimeQueue()
+    print(city_overhead1)
+    q.put(city_overhead1)
+
+    prod = ProducerThread(db.city_db, q)
+    cons = ConsumerThread(q)
 
     threads = []
     prod.start()
